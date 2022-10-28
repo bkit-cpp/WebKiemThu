8ca84a4d3c0bd2ad4e0f376b62f1b441398d8ba9
bkit-cpp <84651107+bkit-cpp@users.noreply.github.com>
2022-10-28 13:29:34 +0700

    upload

---
+++ script2.js
@@ -0,0 +1,95 @@
+
+  
+  
+  function Validator(options) {
+    var selectorRule={};
+    function validate(inputElement,rule) {
+      var errorElement=inputElement.parentElement.querySelector('.input-form p');
+      var errorMessage;
+         var rules=selectorRule[rule.selector];
+        for(var i=0;i<rules.length;i++){
+          errorMessage=rules[i](inputElement.value);
+          if(errorMessage) break;
+        }
+          if(errorMessage)
+          {
+            errorElement.innerText=errorMessage;
+            inputElement.parentElement.classList.add('invalid');
+          }
+          else{
+            errorElement.innerText='';
+            inputElement.parentElement.classList.remove('invalid');
+          }
+       return !errorMessage;
+    }
+   var formelement=document.querySelector(options.form);
+  
+   if(formelement)
+   {
+    formelement.onclick = function(e){
+      e.preventDefault();
+      var isFormValid=true;
+      options.rules.forEach(function(rule){
+        var inputElement=formelement.querySelector(rule.selector);
+        var isValid=validate(inputElement,rule);
+        if(!isValid){
+          isFormValid=false;
+        }
+      });
+      if(isFormValid){
+      window.location="http://localhost:5500/";
+      }
+      else{
+      
+      }
+      
+    }
+    options.rules.forEach(function(rule){
+     if(Array.isArray(selectorRule[rule.selector]))
+     {
+      selectorRule[rule.selector].push(rule.test);
+     }
+     else{
+      selectorRule[rule.selector]=[rule.test];
+     }
+     var inputElement=formelement.querySelector(rule.selector);
+     
+      if(inputElement)
+      {
+        inputElement.onblur=function(){
+          validate(inputElement,rule);
+          
+        }
+      }
+    });
+   console.log(selectorRule);
+   }
+  }
+  Validator.isRequired = function(selector,message){
+   return{
+    selector:selector,
+    test:function(value){
+       return value.trim() ? undefined : message||'The username is blank'
+    }
+   };
+  }
+  Validator.minlenght = function(selector,min,message){
+    return{
+      selector:selector,
+      test:function(value){
+  return value.length >= min? undefined : message||'The pass is so short, min is';
+      }
+     };
+  }
+  Validator.isPasswordConfirm = function(selector,getConfirmValue,message){
+    return{
+      selector:selector,
+      test:function(value){
+        return value===getConfirmValue()? undefined : message|| 'The confirm value is error';
+      }
+     };
+  }
+
+  
+  
+  
